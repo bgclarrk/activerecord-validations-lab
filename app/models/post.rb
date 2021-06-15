f58fffb269573550edbbd9487d8ecc435ba4bffd
+ <+>
2021-06-14 17:48:34 -0700

    Test ActiveRecord validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ActiveRecord::Base
+    include ActiveModel::Validations
+    validates :title, uniqueness: true, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :post_is_clickbait?
+
+    def post_is_clickbait?
+        if title && title.match?(/(Won't Believe)|(Secret)|(Top [0-9])|(Guess)/)
+
+        else
+            errors.add(:title, "Must be clickbait")
+        end
+    end
+
 end
